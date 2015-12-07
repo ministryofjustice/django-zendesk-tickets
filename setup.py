baf3f287325a24682c754f2a44a376744a4c8a3d
Ian Brechin <ian.brechin@gmail.com>
2015-12-07 11:56:37 +0000

    Add python package files

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-zendesk-tickets',
+    version='0.1',
+    packages=['zendesk_tickets'],
+    include_package_data=True,
+    license='MIT License',
+    description='',
+    long_description=README,
+    install_requires=['Django>=1.8,<1.9', 'requests', ],
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Python Developers',
+    ]
+)

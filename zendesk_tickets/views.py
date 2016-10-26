3db8cdae14103338b8f38bb4e66a9293201c943c
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-10-26 12:42:43 +0100

    Accept extra template context in views

--- zendesk_tickets/views.py
+++ zendesk_tickets/views.py
@@ -3,7 +3,6 @@ from urllib.parse import urlparse
 from django.forms.forms import NON_FIELD_ERRORS
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.template.response import TemplateResponse
 from django.utils.http import is_safe_url
 from django.utils.translation import ugettext as _
 from requests.exceptions import HTTPError
@@ -26,7 +25,8 @@ def ticket(request,
            ticket_template_name='zendesk_tickets/ticket.txt',
            form_class=TicketForm,
            subject='Website Ticket',
-           tags=[]):
+           tags=[],
+           extra_context=None):
     if request.method == 'POST':
         form = form_class(request.POST)
         if form.is_valid():
@@ -50,9 +50,15 @@ def ticket(request,
             initial={'referer': return_to}
         )
 
-    return render(request, template_name, {'form': form, 'return_to': return_to})
+    context = {'form': form, 'return_to': return_to}
+    if extra_context:
+        context.update(extra_context)
+    return render(request, template_name, context=context)
 
 
-def success(request, template_name=None):
+def success(request, template_name=None, extra_context=None):
     return_to = get_safe_return_to(request, request.GET.get(RETURN_URL_FIELD))
-    return TemplateResponse(request, template_name, {'return_to': return_to})
+    context = {'return_to': return_to}
+    if extra_context:
+        context.update(extra_context)
+    return render(request, template_name, context=context)

01a6168ce8a7e65ea942e69c568bbf7fdc0882b9
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-12-18 17:12:42 +0000

    Add class-based ticket views superseding function views for easier extensibility and configuration

--- tests/urls.py
+++ tests/urls.py
@@ -1,12 +1,32 @@
 from django.conf.urls import url
+from django.urls import reverse_lazy
 
 from zendesk_tickets import forms, views
 
 urlpatterns = [
+    # class-based views
+    url(r'^ticket/$', views.TicketView.as_view(
+        success_url=reverse_lazy('ticket_sent'),
+        template_name='submit_ticket.html',
+        ticket_subject='Test Feedback',
+        ticket_tags=['feedback', 'test'],
+    ), name='ticket'),
+    url(r'^ticket-with-email/$', views.TicketView.as_view(
+        form_class=forms.EmailTicketForm,
+        success_url=reverse_lazy('ticket_sent'),
+        template_name='submit_ticket.html',
+        ticket_subject='Test Feedback with email address',
+        ticket_tags=['feedback', 'test', 'with-email'],
+    ), name='ticket_with_email'),
+    url(r'^ticket/sent/$', views.TicketSentView.as_view(
+        template_name='success.html',
+    ), name='ticket_sent'),
+
+    # deprecated function views
     url(r'^feedback/$', views.ticket,
         {
             'template_name': 'submit_ticket.html',
-            'success_redirect_url': '',
+            'success_redirect_url': '/',
             'subject': 'Test Feedback',
             'tags': ['feedback', 'test']
         }, name='submit_ticket'),
@@ -14,7 +34,7 @@ urlpatterns = [
         {
             'form_class': forms.EmailTicketForm,
             'template_name': 'submit_ticket.html',
-            'success_redirect_url': '',
+            'success_redirect_url': '/',
             'subject': 'Test Feedback with email address',
             'tags': ['feedback', 'test', 'with-email']
         }, name='submit_ticket_with_email'),

9634c963eb0e819586fd5b962531b12ffb3bcb9e
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-12-19 12:32:45 +0000

    Bump to 0.11 and add minor packaging info

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ import importlib
 import os
 import sys
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-__version__ = importlib.import_module('zendesk_tickets').__version__
+package_info = importlib.import_module('zendesk_tickets')
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
@@ -19,20 +19,24 @@ setup_extensions = importlib.import_module('zendesk_tickets.setup_extensions')
 
 setup(
     name='django-zendesk-tickets',
-    version=__version__,
-    author='Ministry of Justice Digital Services',
+    version=package_info.__version__,
+    author=package_info.__author__,
     url='https://github.com/ministryofjustice/django-zendesk-tickets',
-    packages=find_packages(exclude=['tests']),
+    packages=['zendesk_tickets'],
     include_package_data=True,
     license='MIT',
     description='Django views and forms that submit tickets to Zendesk',
     long_description=README,
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
         'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',

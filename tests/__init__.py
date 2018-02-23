b711003c83406738332d330b57d74b5021af115b
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-02-23 11:37:20 +0000

    Simplify django app testing using only `python setup.py test`

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,22 +1,54 @@
-from django.template import Origin, Template, TemplateDoesNotExist
-from django.template.loaders.app_directories import Loader
+import sys
+
+import django
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+
+test_settings = dict(
+    DEBUG=True,
+    SECRET_KEY='a' * 50,
+    ROOT_URLCONF='tests.urls',
+    INSTALLED_APPS=(
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'zendesk_tickets',
+    ),
+    MIDDLEWARE=[
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+    ],
+    SESSION_ENGINE='django.contrib.sessions.backends.signed_cookies',
+    TEMPLATES=[{
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': False,
+        'OPTIONS': {
+            'context_processors': [],
+            'loaders': ['tests.utils.DummyTemplateLoader'],
+        },
+    }],
+    ZENDESK_BASE_URL='https://zendesk.local/',
+    ZENDESK_API_USERNAME='zendesk_user',
+    ZENDESK_API_TOKEN='api_token',
+    ZENDESK_REQUESTER_ID=111111,
+    ZENDESK_GROUP_ID=222222,
+    ZENDESK_CUSTOM_FIELDS={
+        'referer': 31,
+        'username': 32,
+        'user_agent': 33,
+        'contact_email': 34,
+    },
+)
 
 
-def get_template_source():
-    # mock this function in tests
-    return 'dummy'
+def run():
+    if not settings.configured:
+        settings.configure(**test_settings)
+        django.setup()
+    failures = DiscoverRunner(verbosity=2, failfast=False, interactive=False).run_tests(['tests'])
+    sys.exit(failures)
 
 
-class DummyTemplateLoader(Loader):
-    is_usable = True
-
-    def get_template(self, template_name, template_dirs=None, skip=None):
-        try:
-            return super(DummyTemplateLoader, self).get_template(
-                template_name,
-                skip=skip
-            )
-        except TemplateDoesNotExist:
-            template_source = get_template_source()
-            origin = Origin(template_name, template_name)
-            return Template(template_source, origin, template_name, self.engine)
+if __name__ == '__main__':
+    run()

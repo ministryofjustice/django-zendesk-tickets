11554500f6dc3c02e04cca8cbf9e1f6206022470
Igor <igor.ushkarev@digital.justice.gov.uk>
2020-11-10 22:34:32 +0000

    Move to supporting python3.6+ only as prior versions are have reached end-of-life

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 import importlib
 import os
+import sys
+import warnings
 
 from setuptools import setup
+
+if sys.version_info[0:2] < (3, 6):
+    warnings.warn('This package is tested with Python version 3.6+')
 
 root_path = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(root_path, 'README.rst')) as readme:
     README = readme.read()
 
-install_requires = ['requests']
-setup_requires = ['Django>=2.2,<4']
+install_requires = ['Django>=2.2,<4', 'requests']
 tests_require = ['flake8']
-install_requires += setup_requires
 
 package_info = importlib.import_module('zendesk_tickets')
 setup_extensions = importlib.import_module('zendesk_tickets.setup_extensions')
@@ -38,13 +41,11 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     cmdclass=command_classes,
-    setup_requires=setup_requires,
     install_requires=install_requires,
     tests_require=tests_require,
     test_suite='tests.run',

df59ac309c2f05129b9d7c3a4796be0bdf79f959
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-01-24 20:52:56 +0000

    Add testing support for python 3.9 & 3.10

--- setup.py
+++ setup.py
@@ -47,6 +47,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     cmdclass=command_classes,
     setup_requires=setup_requires,

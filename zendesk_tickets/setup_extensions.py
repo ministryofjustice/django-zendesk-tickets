4a4514dd0dc169c3ac1337b990446f435543355c
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-02-14 13:50:06 +0000

    Simplify setup commands

--- zendesk_tickets/setup_extensions.py
+++ zendesk_tickets/setup_extensions.py
@@ -4,11 +4,8 @@ import os
 import setuptools
 
 
-class SimpleCommand(setuptools.Command):
+class ManagementCommand(setuptools.Command):
     user_options = []
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
     def initialize_options(self):
         pass
@@ -17,33 +14,27 @@ class SimpleCommand(setuptools.Command):
         pass
 
     def run(self):
+        from django.core.management import call_command
+
+        self.announce(self.description, level=distutils.log.INFO)
         cwd = os.getcwd()
-        os.chdir(os.path.dirname(__file__))
-        self.run_command()
-        os.chdir(cwd)
+        try:
+            os.chdir(os.path.dirname(__file__))
+            call_command(self.management_command, **self.management_command_kargs)
+        finally:
+            os.chdir(cwd)
 
-    def run_command(self):
-        raise NotImplementedError
 
-
-class MakeMessages(SimpleCommand):
+class MakeMessages(ManagementCommand):
     description = 'update localisation messages files'
-
-    def run_command(self):
-        from django.core.management import call_command
-
-        self.announce('Updating localisation message files', level=distutils.log.INFO)
-        call_command('makemessages', all=True, no_wrap=True, keep_pot=True)
+    management_command = 'makemessages'
+    management_command_kargs = dict(all=True, no_wrap=True, keep_pot=True)
 
 
-class CompileMessages(SimpleCommand):
+class CompileMessages(ManagementCommand):
     description = 'compile localisation messages files'
-
-    def run_command(self):
-        from django.core.management import call_command
-
-        self.announce('Compiling localisation message files', level=distutils.log.INFO)
-        call_command('compilemessages', fuzzy=False)
+    management_command = 'compilemessages'
+    management_command_kargs = dict(fuzzy=False)
 
 
 command_classes = {

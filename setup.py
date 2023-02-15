e51b72d7968a0a0ba99400ec8e73e6b9ab411061
Igor <igor.ushkarev@digital.justice.gov.uk>
2023-02-15 10:34:04 +0000

    Migrate to declarative build system TODO: message making and compiling is not usable

--- setup.py
+++ setup.py
@@ -1,63 +1,10 @@
 #!/usr/bin/env python
-import importlib
-import os
 import sys
 import warnings
 
 from setuptools import setup
 
 if sys.version_info[0:2] < (3, 8):
-    warnings.warn('This package is tested with Python version 3.8+', stacklevel=1)
+    warnings.warn('This package is only tested on Python version 3.8+', stacklevel=1)
 
-root_path = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(root_path, 'README.rst')) as readme:
-    README = readme.read()
-
-package_info = importlib.import_module('zendesk_tickets')
-setup_extensions = importlib.import_module('zendesk_tickets.setup_extensions')
-command_classes = setup_extensions.command_classes.copy()
-
-setup(
-    name='django-zendesk-tickets',
-    version=package_info.__version__,
-    author=package_info.__author__,
-    author_email='dev@digital.justice.gov.uk',
-    url='https://github.com/ministryofjustice/django-zendesk-tickets',
-    packages=[
-        'zendesk_tickets',
-        'zendesk_tickets.locale.cy.LC_MESSAGES',
-        'zendesk_tickets.locale.en_GB.LC_MESSAGES',
-        'zendesk_tickets.templates.zendesk_tickets',
-    ],
-    include_package_data=True,
-    license='MIT',
-    description='Django views and forms that submit tickets to Zendesk',
-    long_description=README,
-    keywords='zendesk django tickets',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Django',
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 3.1',
-        'Framework :: Django :: 3.2',
-        'Framework :: Django :: 4.0',
-        'Framework :: Django :: 4.1',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-    ],
-    cmdclass=command_classes,
-    python_requires='>=3.6',  # looser requirement than what's tested
-    setup_requires=['Django>=2.2,<4.3'],
-    install_requires=['Django>=2.2,<4.3', 'requests'],
-    tests_require=[],
-    test_suite='tests.run',
-)
+setup()
